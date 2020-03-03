537290e3d5c3a3bd3779094a97af9c645b6abdb9
kalpeshk71 <kalpeshk71@gmail.com>
2020-03-03 13:38:36 +0530

    changed string serializer

--- src/main/java/javaproducer/producer.java
+++ src/main/java/javaproducer/producer.java
@@ -1,7 +1,6 @@
 package javaproducer;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
@@ -18,8 +17,8 @@ public class producer {
 
 
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "StringSerializer");
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "StringSerializer");
 
 
 

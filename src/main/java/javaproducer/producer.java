0a5da1ec197a462cb58e956a6d5f651a2071cd84
kalpeshk71 <kalpeshk71@gmail.com>
2020-03-03 13:43:29 +0530

    changed string serializer

--- src/main/java/javaproducer/producer.java
+++ src/main/java/javaproducer/producer.java
@@ -17,8 +17,8 @@ public class producer {
 
 
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "StringSerializer");
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "StringSerializer");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
 
 
